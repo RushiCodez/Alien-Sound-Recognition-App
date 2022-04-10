5a8cac4735ff21e5a136bfe71052f84d1ac8fb4b
RushiCodez <94935989+RushiCodez@users.noreply.github.com>
2022-04-10 12:02:25 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,9 +9,10 @@ classifier.classify(got_results);
 }
 
 function got_results(error, results){
-    console.error(error);
+if (error){
+console.error(error);
 }
- else{console.log(results);
+ else {console.log(results);
 random_number_r= Math.floor(Math.random()*255)+1;
 random_number_g= Math.floor(Math.random()*255)+1;
 random_number_b= Math.floor(Math.random()*255)+1;
