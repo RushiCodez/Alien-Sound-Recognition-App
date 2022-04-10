0a9766853775c5bd54b13292ba1c837a597d216c
RushiCodez <94935989+RushiCodez@users.noreply.github.com>
2022-04-10 12:12:03 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function startClassification(){
-navigator.mediaDevices.getUserMedia({audio: true})();
+navigator.mediaDevices.getUserMedia({audio: true});
 classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/OKqf3R-9Z/model.json',modelready);
 }
 
@@ -9,13 +9,10 @@ classifier.classify(got_results);
 }
 
 function got_results(error, results){
-if (error){
-console.error(error);
-}
- else {console.log(results);
-random_number_r= Math.floor(Math.random()*255)+1;
-random_number_g= Math.floor(Math.random()*255)+1;
-random_number_b= Math.floor(Math.random()*255)+1;
+    if (error) { console.error(error); 
+    } else { console.log(results); random_number_r = Math.floor(Math.random() * 255) + 1;
+         random_number_g = Math.floor(Math.random() * 255) + 1;
+          random_number_b = Math.floor(Math.random() * 255) + 1;
 
 document.getElementById("result_label").innerHTML = "I can hear-" + results[0].label;
 document.getElementById("result_accuracy").innerHTML = "Accuracy-" + (results[0].confidence*100).toFixed(2)+"%";
